204586576a98f01afdcd0632b3dc26e7c21ab6c2
CS-student-account <dmisyura@alaska.edu>
2020-09-22 01:29:20 -0800

    Wrote comments for names.cpp.

--- HW2/HW2/names/names.cpp
+++ HW2/HW2/names/names.cpp
@@ -16,16 +16,14 @@ using std::cin;
 using std::vector;
 using std::string;
 using std::find;
-using std::reverse;
-using std::copy;
-using std::ostream_iterator;
 using std::sort;
-using std::greater;
 
+//prototype functions
 void InputNames(vector<string> & names);
 bool DoesNameExist(string & nameToFind, vector<string> & names);
 void PrintNames(vector<string> & names);
 
+//main function where the external functions are called
 int main()
 {
 	vector<string> names;
@@ -37,6 +35,7 @@ int main()
 	return 0;
 }
 
+//function that allows strings to be input into a vector
 void InputNames(vector<string> & names)
 {
 	for (int i = 0; i < 10; i++)
@@ -44,11 +43,12 @@ void InputNames(vector<string> & names)
 		string name;
 		cout << "Please enter a name: ";
 		getline(cin, name);
-		names.push_back(name);
+		names.push_back(name); //inserts the input string 'name' into the vector
 	}
 	return;
 }
 
+//function that searches for a specific string within the vector
 bool DoesNameExist(string & nameToFind, vector<string> & names)
 {
 	if (find(names.begin(), names.end(), "David") != names.end())
@@ -63,9 +63,11 @@ bool DoesNameExist(string & nameToFind, vector<string> & names)
 	}
 }
 
+//function that prints an alphabetically sorted version of the vector
 void PrintNames(vector<string> & names)
 {
-	sort(names.begin(), names.end());
+	sort(names.begin(), names.end()); //sorts the strings within the vector
+	cout << "Your names were sorted alphabetically." << endl << endl;
 
 	for (int i = 0; i < 10; i++)
 	{
