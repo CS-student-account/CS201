2b4c04d46e71913630a14514b3427fd6e348a759
CS-student-account <dmisyura@alaska.edu>
2020-09-22 15:43:57 -0800

    Create rice.cpp.

---
+++ HW2/HW2/rice/rice.cpp
@@ -0,0 +1,24 @@
+/*
+* rice.cpp
+* David Misyura
+* 22 Sept 2020
+* rice.cpp for CS201
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::setprecision;
+
+int main()
+{
+	int square;
+	double rice;
+
+	return 0;
+}
