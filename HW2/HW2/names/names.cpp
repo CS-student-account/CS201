4ce12b5883854127cc82733d688e74edf4fab789
CS-student-account <dmisyura@alaska.edu>
2020-09-21 23:38:20 -0800

    function InputNames successfully created.

--- HW2/HW2/names/names.cpp
+++ HW2/HW2/names/names.cpp
@@ -6,14 +6,38 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using std::cout;
 using std::endl;
 using std::cin;
+using std::vector;
+using std::string;
+
+void InputNames(vector<string> & names);
+bool DoesNameExist(const string & nameToFind, const vector<string> & names);
 
 int main()
 {
-
-
-
+	vector<string> names;
+	InputNames(names);
 	return 0;
+}
+
+void InputNames(vector<string> & names)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		string name;
+		cout << "Please enter a name: ";
+		getline(cin, name);
+		names.push_back(name);
+	}
+	return;
+}
+
+bool DoesNameExist(const string & nameToFind, const vector<string> & names)
+{
+
 }
