0707e6bc857772bd74edaedeffd8ab57176cf32b
CS-student-account <dmisyura@alaska.edu>
2020-09-21 22:29:29 -0800

    Create names.cpp

---
+++ HW1/HW1/HW1/HW1.cpp
@@ -0,0 +1,16 @@
+/*
+* HW1.cpp
+* David Misyura
+* 10 Sept 2020
+* HW1 for CS201
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+	return 0;
+}
---
+++ HW2/HW2/names/names.cpp
@@ -0,0 +1,19 @@
+/*
+* names.cpp
+* David Misyura
+* 21 Sept 2020
+* names.cpp for CS201
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+
+
+
+	return 0;
+}
