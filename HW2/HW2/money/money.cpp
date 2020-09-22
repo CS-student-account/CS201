f77136871e8e845fd83eaa0c6f285dce0e733805
CS-student-account <dmisyura@alaska.edu>
2020-09-22 01:34:57 -0800

    Create money.cpp.

---
+++ HW2/HW2/money/money.cpp
@@ -0,0 +1,17 @@
+/*
+* money.cpp
+* David Misyura
+* 21 Sept 2020
+* money.cpp for CS201
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+
+	return 0;
+}
