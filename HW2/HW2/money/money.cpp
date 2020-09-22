f7492244e8e1d45f9729f2303a71a64ab5281221
CS-student-account <dmisyura@alaska.edu>
2020-09-22 02:23:23 -0800

    Basic function of money.cpp is complete. It will prompt for and report back the number of each type of currency.

--- HW2/HW2/money/money.cpp
+++ HW2/HW2/money/money.cpp
@@ -6,12 +6,36 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using std::cout;
 using std::endl;
 using std::cin;
 
 int main()
 {
+	int pennies;
+	int nickels;
+	int dimes;
+	int quarters;
+	int dollars;
 
+	cout << "How many pennies? ";
+	cin >> pennies;
+	cout << "How many nickels? ";
+	cin >> nickels;
+	cout << "How many dimes? ";
+	cin >> dimes;
+	cout << "How many quarters? ";
+	cin >> quarters;
+	cout << "How many dollars? ";
+	cin >> dollars;
+
+	cout << endl << "You have " << pennies << " pennies." << endl;
+	cout << "You have " << nickels << " nickels." << endl;
+	cout << "You have " << dimes << " dimes." << endl;
+	cout << "You have " << quarters << " quarters." << endl;
+	cout << "You have " << dollars << " dollars." << endl;
 	return 0;
 }
