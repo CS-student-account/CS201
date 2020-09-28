fce241e66131b600362af1d97fadecbffa5282a9
CS-student-account <dmisyura@alaska.edu>
2020-09-27 22:36:16 -0800

    Create boxer.h and boxer.cpp.

---
+++ HW3/HW3/boxprint/boxer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+int boxer(string inputString, int inputInt)
+{
+	return 0;
+}
---
+++ HW3/HW3/boxprint/boxer.h
@@ -0,0 +1,7 @@
+#ifndef FILE_BOXER_HPP_INCLUDED
+#define FILE_BOXER_HPP_INCLUDED
+#include <string>
+
+int boxer(std::string inputString, int inputInt);
+
+#endif 
