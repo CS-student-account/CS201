29f0e64d5f032d7386d544631251a3ef0815f6a1
CS-student-account <dmisyura@alaska.edu>
2020-09-30 20:42:46 -0800

    Completed kelvin.cpp and provided comments. Provided comments for faulty code as well.

--- HW3/HW3/kelvin/kelvin.cpp
+++ HW3/HW3/kelvin/kelvin.cpp
@@ -10,18 +10,48 @@
 using std::cout;
 using std::cin;
 using std::endl;
-using std::string;
 
 double ctok(double c)
 {
+	if (c <= -273.15) //produce error if user inputs below zero kelvin
+	{
+		cout << endl << "Please don't enter -273.15 degrees celsius or below. Try again later."
+			<< endl;
+	}
+	else
+	{
+		int k = c + 273.15;
+		return k;
+	}
+}
+
+int main()
+{
+	double c;
+	cout << "This program converts celsius to kelvin." << endl 
+		<< "Please enter a temperature in celsius you wish to convert to kelvin: ";
+	cin >> c;
+	double k = ctok(c);
+	if (!c <= -273.15) //only converts celsius to kelvin if greater than zero kelvin
+	{
+		cout << c << " degrees celsius converted to kelvin is " << k << endl;
+	}
+	return 0;
+}
+
+/*original, faulty code with comments
+
+double ctok(double c) //will produce error when given -273.15 C or below
+{
 	int k = c + 273.15;
-	return int;
+	return int; //should be "return k" instead
 }
 
 int main()
 {
 	double c = 0;
-	cin >> d;
-	double k = ctok("c");
-	Cout << k << '/n';
-}
+	cin >> d; //should be "cin >> c" instead
+	double k = ctok("c"); //there shouldn't be apostrophes around c
+	Cout << k << '/n'; //cout should be lowercase, and '/n' can
+	                   be replaced with endl
+}*/
