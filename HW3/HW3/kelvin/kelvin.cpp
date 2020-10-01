50a34346a482fc69d74b28494391861b09d93749
CS-student-account <dmisyura@alaska.edu>
2020-09-30 20:09:51 -0800

    Created kelvin.cpp with its original, faulty code.

---
+++ HW3/HW3/kelvin/kelvin.cpp
@@ -0,0 +1,27 @@
+/*
+* kelvin.cpp
+* David Misyura
+* 29 Sept 2020
+* kelvin.cpp for CS201
+*/
+
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+double ctok(double c)
+{
+	int k = c + 273.15;
+	return int;
+}
+
+int main()
+{
+	double c = 0;
+	cin >> d;
+	double k = ctok("c");
+	Cout << k << '/n';
+}
