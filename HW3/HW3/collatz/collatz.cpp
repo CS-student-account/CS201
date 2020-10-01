5e7759a211912ae06258ca87093fa59f78e7d25d
CS-student-account <dmisyura@alaska.edu>
2020-09-30 19:07:52 -0800

    Completed collatz.cpp and provided comments

--- HW3/HW3/collatz/collatz.cpp
+++ HW3/HW3/collatz/collatz.cpp
@@ -14,5 +14,32 @@ using std::string;
 
 int main()
 {
+	int input = 0;
+
+	while (input <= 0) //prompt the user for a positive integer
+	{
+		cout << endl << "Enter a positive integer for the collatz sequence: ";
+		cin >> input;
+
+		if (input <= 0) //indicate that the user's integer wasn't positive
+		{
+			cout << endl << "Your integer wasn't positive, try again." << endl;
+		}
+	}
+	cout << endl << input;
+
+	while (input != 1) //perform the calculations of the collatz sequence
+	{
+		if (input % 2) //for even integers
+		{
+			input = (3 * input) + 1;
+		}
+		else //for odd integers
+		{
+			input /= 2;
+		}
+		cout << " " << input;
+	}
+	cout << endl;
 	return 0;
 }
