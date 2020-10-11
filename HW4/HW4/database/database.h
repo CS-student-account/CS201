3217a07cd11ef71869689802da8db9bfa0fade76
CS-student-account <dmisyura@alaska.edu>
2020-10-11 00:55:04 -0800

    database.h filled out with relevant functions and libraries

--- HW4/HW4/database/database.h
+++ HW4/HW4/database/database.h
@@ -4,6 +4,21 @@
 #include <string>
 #include <map>
 
+struct MyDatabaseRecord
+{
+	// replace this with information related to your database 
+	std::string objectName{"obj"}; 
+	std::string materialName{ "mtl" };
+	std::string diffuseColor{ "diff" };
+	std::string specularColor{ "spec" };
+	bool twoSided{ false };
+};
 
+bool CreateRecord(const std::string &key);
+bool ReadRecord(const std::string &key, MyDatabaseRecord &record);
+bool UpdateRecord(const std::string &key, const MyDatabaseRecord &record); 
+bool DeleteRecord(const std::string &key);
+bool InputRecord(MyDatabaseRecord &record); 
+bool PrintRecord(const std::string & key);
 
 #endif 
