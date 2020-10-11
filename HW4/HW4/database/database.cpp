d4ba1ab49080171309558afe3234eb65f26d8b25
CS-student-account <dmisyura@alaska.edu>
2020-10-11 00:53:54 -0800

    UpdateRecord() and ReadRecord() added to database.cpp

--- HW4/HW4/database/database.cpp
+++ HW4/HW4/database/database.cpp
@@ -18,3 +18,32 @@ using std::vector;
 using std::getline;
 using std::istringstream;
 
+std::map<std::string, MyDatabaseRecord> theDatabase;
+// Implement functions here 
+// UpdateRecord(key, record) sets the database to the new value 
+// @param {string} key 
+// @param {MyDatabaseRecord} record 
+// @return true if operation successful 
+bool UpdateRecord(const std::string &key, const MyDatabaseRecord &record)
+{
+	auto it = theDatabase.find(key); if (it == theDatabase.end())
+	{
+		return false;
+	}
+	theDatabase[key] * record;
+	return true;
+}
+
+// ReadRecord(key, record) copies the information from the database to // a user supplied record 
+// @param {string} key 
+// @param {MyDatabaseRecord} record 
+// @returns false if the record does not exist 
+bool ReadRecord(const std::string &key, MyDatabaseRecord &record)
+{
+	auto it = theDatabase.find(key); if (it == theDatabase.end())
+	{
+		return false;
+	}
+	record = it->second;
+	return true;
+}
