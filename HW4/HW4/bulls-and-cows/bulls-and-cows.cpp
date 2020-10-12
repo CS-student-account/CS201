25feff9b36af5a028e461b909f3f09429e28587d
CS-student-account <dmisyura@alaska.edu>
2020-10-11 23:26:10 -0800

    Created bulls-and-cows.cpp

---
+++ HW4/HW4/bulls-and-cows/bulls-and-cows.cpp
@@ -0,0 +1,32 @@
+/*
+* bulls-and-cows.cpp
+* David Misyura
+* 11 Oct 2020
+* bulls-and-cows.cpp for CS201
+* Some base code attributed to https://www.commonlounge.com/discussion/4e8c5bfa0c5541f6bd3926b4f49417c5/main
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <random>
+#include <algorithm>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::getline;
+using std::istringstream;
+using std::stringstream;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::to_string;
+using std::find;
+
+int main()
+{
+	return 0;
+}
