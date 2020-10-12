033b335ff07842a2947f3dd47733576685c3c991
CS-student-account <dmisyura@alaska.edu>
2020-10-11 23:27:38 -0800

    Implemented countBulls() and countCows() in bulls-and-cows.cpp

--- HW4/HW4/bulls-and-cows/bulls-and-cows.cpp
+++ HW4/HW4/bulls-and-cows/bulls-and-cows.cpp
@@ -26,7 +26,81 @@ using std::uniform_int_distribution;
 using std::to_string;
 using std::find;
 
+int countBulls(string &answer, string &guess, bool answerDigitUsed[], bool guessDigitUsed[])
+{
+	int count = 0;
+
+	for (int i=0; i < 4; i++)
+	{
+		if (answer[i] == guess[i])
+		{
+			count++;
+			answerDigitUsed[i] = true;
+			guessDigitUsed[i] = true;
+		}
+	}
+	return count;
+}
+
+int countCows(string &answer, string &guess, bool answerDigitUsed[], bool guessDigitUsed[])
+{
+	int count = 0;
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (answerDigitUsed[i] || guessDigitUsed[j])
+			{
+				continue;
+			}
+
+			if (answer[i] == guess[j])
+			{
+				count++;
+				answerDigitUsed[i] = true;
+				guessDigitUsed[j] = true;
+			}
+		}
+	}
+	return count;
+}
+
 int main()
 {
+	random_device device;
+	mt19937 generator(device());
+	uniform_int_distribution<int> distribution(0, 9);
+	vector<int> answerIntVector;
+
+	while (answerIntVector.size() != 4)
+	{
+		int distributionInt = distribution(generator);
+		if (find(answerIntVector.begin(), answerIntVector.end(), distributionInt) == answerIntVector.end())
+		{
+			answerIntVector.push_back(distributionInt);
+		}
+	}
+
+	stringstream answerIntVectorStream;
+	for (size_t i = 0; i < answerIntVector.size(); ++i)
+	{
+		answerIntVectorStream << answerIntVector[i];
+
+	}
+
+	string answerString = answerIntVectorStream.str();
+	istringstream answerStream(answerString);
+	int answerInt;
+	answerStream >> answerInt;
+	string guess;
+	cout << endl << "Welcome to the game of Bulls and Cows." << endl;
+	cout << endl << "In this game, you must guess a random, 4-digit number consisting of unique numbers." << endl;
+	cout << endl << "When you correctly guess the correct number in the right position, that's a bull." << endl;
+	cout << endl << "When you correctly guess the correct number but in the wrong position, that's a cow." << endl;
+	cout << endl << "You can also enter -1 to see the solution. Good luck." << endl << endl;
+
+
+
 	return 0;
 }
