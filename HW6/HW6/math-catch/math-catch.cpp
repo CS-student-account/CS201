0668964c49a812e4f8abc4fe3720fff17238c58c
CS-student-account <dmisyura@alaska.edu>
2020-11-12 19:10:47 -0900

    Implemented Sine() in math-catch.cpp

--- HW6/HW6/math-catch/math-catch.cpp
+++ HW6/HW6/math-catch/math-catch.cpp
@@ -1,25 +1,25 @@
-/*
-* math-catch.cpp
-* David Misyura
-* 12 Nov 2020
-* math-catch.cpp for CS201
-*/
-
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include <iostream>
-#include <iomanip>
-#include <string>
-#include <map>
-#include <random>
 #include <cmath>
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
-using std::getline;
+using std::sin;
 
-int main()
+unsigned int Factorial(unsigned int number) 
 {
-    
+	return number <= 1 ? number : Factorial(number - 1)*number;
+}
+
+unsigned int Sine(unsigned int number)
+{
+	int x = number;
+	return sin(x);
+}
+
+TEST_CASE("Factorials are computed", "[factorial]") 
+{
+	REQUIRE(Factorial(1) == 1);
+	REQUIRE(Factorial(2) == 2);
+	REQUIRE(Factorial(3) == 6);
+	REQUIRE(Factorial(10) == 3628800);
+	REQUIRE(Sine(3) == 35);
 }
