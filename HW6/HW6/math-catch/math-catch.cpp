2b75f7e04b774ce2524d38925b751e55b1e9c372
CS-student-account <dmisyura@alaska.edu>
2020-11-12 17:43:51 -0900

    Created math-catch.cpp

---
+++ HW6/HW6/math-catch/math-catch.cpp
@@ -0,0 +1,23 @@
+/*
+* math-catch.cpp
+* David Misyura
+* 12 Nov 2020
+* math-catch.cpp for CS201
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::getline;
+int main()
+{
+    
+}
