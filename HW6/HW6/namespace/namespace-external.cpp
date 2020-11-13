c2d937c2f937acb931a7e700340c1e0ee325cfcf
CS-student-account <dmisyura@alaska.edu>
2020-11-12 15:12:14 -0900

    Created namespace-external.cpp

---
+++ HW6/HW6/namespace/namespace-external.cpp
@@ -0,0 +1,28 @@
+/*
+* namespace-external.cpp
+* David Misyura
+* 12 Nov 2020
+* namespace-external.cpp for CS201
+*/
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <random>
+#include <algorithm>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Output.H>
+#include <FL/Fl_Box.H>
+using std::string;
+using std::stringstream;
+using std::vector;
+using std::getline;
+using std::istringstream;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::to_string;
+using std::find;
