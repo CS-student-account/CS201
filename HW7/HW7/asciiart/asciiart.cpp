8e8a3aba022dd91025d92d89e0d59beb27920cf2
CS-student-account <dmisyura@alaska.edu>
2020-11-29 23:53:19 -0900

    Created main() in asciiart.cpp

--- HW7/HW7/asciiart/asciiart.cpp
+++ HW7/HW7/asciiart/asciiart.cpp
@@ -1,20 +1,90 @@
-// asciiart.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+* aciiart.cpp
+* David Misyura
+* 29 Nov 2020
+* aciiart.cpp for CS201
+*/
 
+#include <fstream>
+using std::ifstream;
 #include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
+using std::getline;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	ifstream fin("parrot.ppm");
+	if (!fin) 
+	{
+		cout << "Error opening parrot.ppm" << endl;
+		exit(1);
+	}
+	cout << "Opened parrot.ppm" << endl;
+
+	// read the magic number
+	string line;
+	getline(fin, line);
+	if (line[0] == 'P' && line[1] == '3') 
+	{
+		cout << "PPM (text)" << endl;
+	}
+	else 
+	{
+		cout << "Unable to read magic number P3" << endl;
+		exit(2);
+	}
+
+	// quick and dirty - process the comment
+	getline(fin, line);
+	if (line[0] == '#') 
+	{
+		cout << "Ignoring comment" << endl;
+	}
+
+	// read the image size xres x yres
+	int xres, yres, maxval;
+	fin >> xres >> yres >> maxval;
+	if (!fin) 
+	{ 
+		cout << "Error reading" << endl; exit(1); 
+	}
+	cout << "Image size " << xres << "x" << yres << endl;
+	cout << "Maxval = " << maxval << endl;
+
+	int r, g, b, y;
+	int pixels2read = xres * yres;
+	for (int i = 0; i < pixels2read; i++)
+	{
+		fin >> r >> g >> b;
+		//    cout << "<" << r << "," << g << "," << b << ">";
+			// Implement Y = 0.2126R + 0.7152G + 0.0722B
+		y = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+		//    cout << " -> " << y;
+			// Make sure y is [0, 255]
+		if (y < 0 || y>255) 
+		{
+			cout << "ERROR: y out of range" << endl;
+			exit(6);
+		}
+		// map y to a character
+		const char values[] = " .-+*@0#";
+		// values [0, 7] and y [0, 255] so / by 32
+		int val_map = y / 32;
+		//    cout << " -> " << val_map << " -> " << values[val_map] << endl;
+
+		cout << values[val_map];
+		if (i % 80 == 79) cout << endl;
+
+	}
+
+	// finish reading - store R,G,B into a vector
+	// move read to a function
+	// create a class for PPM files
+	// HW7 - convert to grayscale (R,G,B) -> Gray
+	// HW7 - convert Gray to ASCII char
+
+	cout << "Done" << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
