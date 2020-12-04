5e7d8a028ba7a17de72222b5781aa788074ee5ee
CS-student-account <dmisyura@alaska.edu>
2020-12-03 22:59:12 -0900

    Created Environment.cpp

---
+++ HW8/HW8/thermostat/Environment.cpp
@@ -0,0 +1,17 @@
+/*
+* Environment.cpp
+* David Misyura
+* 3 Dec 2020
+* Environment.cpp for CS201
+*/
+
+#include "Environment.h"
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+
+Environment::Environment()
+{}
