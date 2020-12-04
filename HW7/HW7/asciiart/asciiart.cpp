dcbef65f2649f20f603fc091f98512318812501f
CS-student-account <dmisyura@alaska.edu>
2020-12-03 18:36:02 -0900

    Commented in asciiart.cpp

--- HW7/HW7/asciiart/asciiart.cpp
+++ HW7/HW7/asciiart/asciiart.cpp
@@ -9,19 +9,15 @@
 #include "Image3.hpp"
 #include <fstream>
 #include <iostream>
-#include <string>
 using std::cout;
-using std::endl;
-using std::string;
-using std::getline;
 using std::ifstream;
 
 int main()
 {
-	ifstream fin("parrot.ppm");
-	Image3 image(80,80);
-	fin >> image;
-	image.printASCII(cout);
+	ifstream fin("parrot.ppm"); //manually load in a standard PPM image
+	Image3 image(80,80); //create an Image3 object and give it the image's dimensions
+	fin >> image; //insert the file input stream into the Image3 object for processing
+	image.printASCII(cout); //call the member function of the image responsible for printing
 
 	return 0;
 }
