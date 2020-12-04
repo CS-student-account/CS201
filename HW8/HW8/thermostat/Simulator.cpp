e4ed3ded8a68f49ca2842bec76a5de2a9aa67511
CS-student-account <dmisyura@alaska.edu>
2020-12-03 23:04:08 -0900

    Created Simulator.cpp

---
+++ HW8/HW8/thermostat/Simulator.cpp
@@ -0,0 +1,17 @@
+/*
+* Simulator.cpp
+* David Misyura
+* 3 Dec 2020
+* Simulator.cpp for CS201
+*/
+
+#include "Simulator.h"
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+
+Simulator::Simulator()
+{}
